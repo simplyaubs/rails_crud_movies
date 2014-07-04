0486f978cd0147ce27c4a296c45ae428597d1b14
Aubrey Howell <simplyaubs@gmail.com>
2014-07-03 22:10:20 -0600

    User can update a movie from list.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -15,6 +15,21 @@ class MoviesController < ApplicationController
     end
   end
 
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    @movie.update_attributes!(movie_params)
+
+    redirect_to movies_path
+  end
+
   private
   def movie_params
     params.require(:movie).permit(:title, :rating)
--- spec/features/movies_spec.rb
+++ spec/features/movies_spec.rb
@@ -11,4 +11,26 @@ feature 'CRUD movies' do
     expect(page).to have_content 'Frozen'
     expect(page).to have_content 3
   end
+
+  scenario 'User can update a movie from list' do
+    visit '/'
+    expect(page).to have_content 'Welcome'
+    click_on 'Add a movie'
+    fill_in 'Title', with: 'Frozen'
+    fill_in 'Rating', with: 3
+    click_on 'Add movie'
+    expect(page).to have_content 'Frozen'
+    expect(page).to have_content 3
+    click_on 'Frozen'
+    expect(page).to have_content 'Frozen'
+    expect(page).to have_content 3
+    click_on 'Edit'
+    fill_in 'Title', with: 'The Princess Bride'
+    fill_in 'Rating', with: 10
+    click_on 'Update movie'
+    expect(page).to have_content 'The Princess Bride'
+    expect(page).to have_content 10
+    expect(page).to_not have_content 'Frozen'
+    expect(page).to_not have_content 3
+  end
 end
